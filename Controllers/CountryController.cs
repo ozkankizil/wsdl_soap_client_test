154305b09ed28c226cd71096d6b19da129cb048a
Özkan Kızıl <kizilozkan@gmail.com>
2025-06-26 16:04:40 +0300

    init commit

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,37 @@
+﻿using CountryInfoService;
+using Microsoft.AspNetCore.Mvc;
+
+public class CountryController : Controller
+{
+    private readonly CountryInfoServiceSoapTypeClient _client;
+
+    public CountryController()
+    {
+        _client = new CountryInfoServiceSoapTypeClient(
+            CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var countries = await _client.ListOfCountryNamesByCodeAsync();
+        var list = countries.Body.ListOfCountryNamesByCodeResult;
+
+        ViewBag.Countries = list;
+        return View();
+    }
+
+    [HttpPost]
+    [Route("Country/GetCapital")]
+    public async Task<IActionResult> GetCapital(string code)
+    {
+        var capital = await _client.CapitalCityAsync(code);
+        ViewBag.Capital = capital.Body.CapitalCityResult;
+
+        // Yeniden ülke listesini yollayalım
+        var countries = await _client.ListOfCountryNamesByCodeAsync();
+        ViewBag.Countries = countries.Body.ListOfCountryNamesByCodeResult;
+
+        return View("Index");
+    }
+}
+
